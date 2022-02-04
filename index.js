bb2acb546db32b185a5b9f33c02a5cc2c92d12c8
Nekell Ganteng <93813872+Nekellganss-Id@users.noreply.github.com>
2022-02-04 18:52:44 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -628,6 +628,111 @@ let mengmeme = await alpha.sendImageAsSticker(m.chat, upz, m, { packname: global
 await fs.unlinkSync(mengmeme)
 }
 break
+case 'sewabot': case 'sewa': case 'buypremium': {
+                alpha.sendMessage(m.chat, { image: { url: 'https://k.top4top.io/p_2226byghn0.jpg' }, caption: `*Hai Idoll ${m.pushName}*\n\n Bot Rental Prices\n⭔ 15k Per Group via Dana 1 Month\n⭔ 20k via pulsa 1 Month\n\n Premium Price Bot\n⭔ 10K per User 1 bulan\n\nPayment can be via Dana/Pulsa\n\nFor more details, you can chat with the owner\nhttps://wa.me/6289695073357 (Owner)\n\nDonate For Me : \n\n⭔ Dana : 089695073357\n⭔ Pulsa : 081224413305` }, { quoted: m })
+            }
+            break
+ case 'jodohku': case 'jodohgw': {
+            if (!m.isGroup) throw mess.group
+            let member = participants.map(u => u.id)
+            let me = m.sender
+            let jodoh = member[Math.floor(Math.random() * member.length)]
+            let jawab = `👫Jodoh mu adalah
+
+@${me.split('@')[0]} ❤️ @${jodoh.split('@')[0]}`
+            alpha.sendTextWithMentions(m.chat, jawab, m)
+            }
+            break
+            case 'jadian': case 'ewean': {
+            if (!m.isGroup) throw mess.group
+            let member = participants.map(u => u.id)
+            let orang = member[Math.floor(Math.random() * member.length)]
+            let jodoh = member[Math.floor(Math.random() * member.length)]
+            let jawab = `Ciee yang Jadian💖 Jangan lupa pajak jadiannya🐤
+
+@${orang.split('@')[0]} ❤️ @${jodoh.split('@')[0]}`
+            alpha.sendTextWithMentions(m.chat, jawab, m)
+            }
+            break
+case 'imagenobg': case 'removebg': case 'remove-bg': {
+	    if (!quoted) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+	    if (!/image/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+	    if (/webp/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+	    let remobg = require('remove.bg')
+	    let apirnobg = ['q61faXzzR5zNU6cvcrwtUkRU','S258diZhcuFJooAtHTaPEn4T','5LjfCVAp4vVNYiTjq9mXJWHF','aT7ibfUsGSwFyjaPZ9eoJc61','BY63t7Vx2tS68YZFY6AJ4HHF','5Gdq1sSWSeyZzPMHqz7ENfi8','86h6d6u4AXrst4BVMD9dzdGZ','xp8pSDavAgfE5XScqXo9UKHF','dWbCoCb3TacCP93imNEcPxcL']
+	    let apinobg = apirnobg[Math.floor(Math.random() * apirnobg.length)]
+	    hmm = await './src/remobg-'+getRandom('')
+	    localFile = await alpha.downloadAndSaveMediaMessage(quoted, hmm)
+	    outputFile = await './src/hremo-'+getRandom('.png')
+	    m.reply(mess.wait)
+	    remobg.removeBackgroundFromImageFile({
+	      path: localFile,
+	      apiKey: apinobg,
+	      size: "regular",
+	      type: "auto",
+	      scale: "100%",
+	      outputFile 
+	    }).then(async result => {
+	    alpha.sendMessage(m.chat, {image: fs.readFileSync(outputFile), caption: mess.success}, {quoted:m})
+	    await fs.unlinkSync(localFile)
+	    await fs.unlinkSync(outputFile)
+	    })
+	    }
+	    break
+	    case 'setcmd': {
+global.db.data.cmd = global.db.data.cmd || {}
+if (!m.quoted)return reply(`Reply stiker!!\nExample : ${prefix + command} menu\n\n\n*Note : Tidak dapat disertai Prefix!!*`)
+if (!m.quoted.fileSha256)return reply('SHA256 Hash Missing')
+if (!q)return reply(`Untuk Command Apa?`)
+let sticker = global.db.data.cmd
+let hash = m.quoted.fileSha256.toString('base64')
+if (sticker[hash] && sticker[hash].locked) return reply('You have no permission to change this sticker command')
+sticker[hash] = {
+q,
+mentionedJid: m.mentionedJid,
+creator: m.sender,
+at: + new Date,
+locked: false,
+}
+reply(`Done!`)
+}
+break
+case 'delcmd': {
+let hash = m.quoted.fileSha256.toString('base64')
+if (!hash)return reply(`Tidak ada hash`)
+let sticker = global.db.data.cmd
+if (sticker[hash] && sticker[hash].locked)return reply('You have no permission to delete this sticker command')
+delete sticker[hash]
+reply(`Done!`)
+}
+break
+case 'listcmd': {
+let teks = `
+*List Hash*
+Info: *bold* hash is Locked
+
+*Hash :*
+ ${Object.entries(global.db.data.cmd).map(([key, value], index) => `${index + 1}. ${value.locked ? `*${key}*` : key} 
+*Command: ${value.q}*
+*Creator : @${value.creator.split("@")[0]}*
+*Create Time : ${moment(value.at * 1).tz('Asia/Jakarta').format('DD/MM/YYYY HH:mm:ss')}*
+*Locked : ${value.locked}*
+`).join('\n')}
+`.trim()
+alpha.sendTextWithMentions(m.chat, teks, m)
+}
+break
+case 'lockcmd': {
+if (!isCreator)return reply('Only Onwer..')
+if (!m.quoted)return reply('Reply Pesan!')
+if (!m.quoted.fileSha256)return reply('SHA256 Hash Missing')
+let sticker = global.db.data.cmd
+let hash = m.quoted.fileSha256.toString('base64')
+if (!(hash in sticker))return reply('Hash not found in database')
+sticker[hash].locked = !/^un/i.test(command)
+reply('Done!')
+}
+break
             case 'join': {
                 if (!isCreator) throw mess.owner
                 if (!text) throw 'Masukkan Link Group!'
